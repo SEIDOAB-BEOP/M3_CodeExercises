bb7efa2295b49c570739cbea7fe4a48f71037b96
Martin <martin_lenart@icloud.com>
2023-09-25 13:28:15 +0200

    update

--- 02_Friends/Program.cs
+++ 02_Friends/Program.cs
@@ -17,7 +17,6 @@ public class csFriend
 
     public string Email { get; set; }
     public enFriendLevel Level { get; set; }
-    public DateTime? BirthDay { get; set; } = null
 }
 
 class Program
@@ -25,6 +24,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, Friends!");
+
+        #region how create a Random Name and Email address
+        var rnd = new csSeedGenerator();
+
+        //A random Name
+        string _firstName = rnd.FirstName;
+        string _lastName = rnd.LastName;
+        Console.WriteLine($"{_firstName} {_lastName}");
+
+        //A random email address
+        Console.WriteLine(rnd.Email(_firstName, _lastName));
+        #endregion
     }
 }
 
@@ -36,5 +47,11 @@ class Program
 //   Create an instance of csFriend() setting the properties to random values.
 //3. Modify the setter in Name so an Error is thrown if "" null is begin
 //   set as Name
-//4. Create a method ToString() in csFriends that presents the instance of csFriend.
+
+//4. Create a method ToString() in csFriend that presents the instance of csFriend.
+//   For example "Sam Baggins is my BestFriend and can be reached at sam.baggins@gmail.com"
+//5. Create an array of 10 random instances of csFriend and have them present themself
+
+//6. Add a csCar as a property, Car, to csFriend class. Instantiate a csFriend
+//   as a variable friend and give your friend a car.
 
