eec6aa9098dad75eb71b702025c994cd877d41b9
Martin <martin_lenart@icloud.com>
2023-09-25 16:26:02 +0200

    update

--- 02_Friends/Program.cs
+++ 02_Friends/Program.cs
@@ -43,15 +43,20 @@ class Program
 //1. Create a constructor to class csFriend that takes Name, Email, and Level as
 //   Parameters and sets the corresponding properties.
 //   Create an instance of csFriend(..) settign the properties with Arguments
+
 //2. Create an empty constructor that sets all properties to random values
 //   Create an instance of csFriend() setting the properties to random values.
-//3. Modify the setter in Name so an Error is thrown if "" null is begin
-//   set as Name
 
-//4. Create a method ToString() in csFriend that presents the instance of csFriend.
+//3. Create a method ToString() in csFriend that presents the instance of csFriend.
 //   For example "Sam Baggins is my BestFriend and can be reached at sam.baggins@gmail.com"
-//5. Create an array of 10 random instances of csFriend and have them present themself
 
-//6. Add a csCar as a property, Car, to csFriend class. Instantiate a csFriend
-//   as a variable friend and give your friend a car.
+//Advanced:
+//4. Create an array of 10 random instances of csFriend and have them present themself
+
+//5. Add a property, Car, of type csCar to csFriend class. Instantiate a csFriend
+//   as a variable friend and give your friend a random car.
+
+//6. Modify ToString() in csFriend to also present the friends car if it exists (not null)
+
+//7. Modify the setter in Name so an Error is thrown if the new name is null or ""
 
