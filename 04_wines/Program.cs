ac5489e560367b486a8ec7233d6f02b2cfa3f6ed
Martin <martin_lenart@icloud.com>
2023-09-26 10:26:00 +0200

    added wines

---
+++ 04_wines/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04_wines;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+//Exercise:
+// 1. Modellera en flaska vin en C# class. Utmärkande för ett vin är
+//    Druva: Reissling, Tempranillo, Chardonay, Shiraz, Cabernet Savignoin, Syrah
+//    Typ: Rött, vitt, rose
+//    Namn: namnet på vinet
+//    Land: Tyskland, Frankrike, Spanien
+//    Pris:
+//
+// 2. När vinet väl är skapad så ska man bara kunna ändra pris.
+//
+// 3. Skapa en ToString i din vinklass som presenterar vinet.
+//
+// 4. Skapa en vinkällare bestående av 10 flaskor av slumpmässig Druva,
+//    Typ, Namn, Land och pris
+//
+// 5. Vilket är det billigaste och dyraste vinet i vinkällaren?
+//
+// 7. Vad är värdet av vinkällaren?
