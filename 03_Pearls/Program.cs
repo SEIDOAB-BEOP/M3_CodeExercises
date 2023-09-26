c6ad30d138d9407a2b55d7bde1adeb4c8b3cebe3
Martin <martin_lenart@icloud.com>
2023-09-26 09:55:58 +0200

    added pearls

---
+++ 03_Pearls/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _03_Pearls;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+//Exercise:
+// 1. Modellera en pärlan i en C# class. Utmärkande för en pärla är
+//    Storlek: Diameter 5mm till 25mm
+//    Färg: Svart, Vit, Rosa
+//    Form: Rund, Droppformad
+//    Typ: Sötvatten, Saltvatten
+//
+// 2. Skapa en ToString i din pärlklass som presenterar pärlan.
+//
+// 3. Skapa ett halsband bestående av 10 pärlor av slumpmässig storlek, färg
+//    form, och typ
+//
+// 4. Vilken färg, form och typ har den minsta och den största pärlan i halsbandet?
+//
+
+
+
