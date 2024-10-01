12e405b8566c7320190b4e461d933981cfc2099f
Martin <martin_lenart@icloud.com>
2024-10-01 08:10:36 +0200

    update

--- 06_Wines_Interfaces/Models/csWine.cs
+++ 06_Wines_Interfaces/Models/csWine.cs
@@ -3,12 +3,12 @@ using Seido.Utilities.SeedGenerator;
 
 namespace Models
 {
-	public class csWine //: IWine
-	{
-        // public override string ToString()
-        //     => $"Wine {Name} from {Country} is {WineType} and made from grapes {GrapeType}. The price is {Price:N2} Sek";
-        public csWine(csSeedGenerator _seeder)
-        {}
-	}  
+        public class csWine //: IWine
+        {
+                // public override string ToString()
+                //     => $"Wine {Name} from {Country} is {WineType} and made from grapes {GrapeType}. The price is {Price:N2} Sek";
+                public csWine(csSeedGenerator _seeder)
+                {}
+        }  
 }
 
