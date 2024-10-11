36378263213cd081819d7deff09d2cc788f8b018
Martin <martin_lenart@icloud.com>
2024-10-11 13:55:57 +0200

    09_records

--- 06_Wines_Interfaces/Models/csWine.cs
+++ 06_Wines_Interfaces/Models/csWine.cs
@@ -12,6 +12,6 @@ namespace Models
                 public csWine (csSeedGenerator _seeder)
                 {
                 }
-        }  
+        }
 }
 
---
+++ 09_records/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Seido.Utilities.SeedGenerator;
+
+Console.WriteLine("Hello Pearls!");
+var _seeder = new csSeedGenerator();
+
+//Exercise:
+// 1. Modellera en pärlan i en C# class. Utmärkande för en pärla är
+//    Storlek: Diameter 5mm till 25mm
+//    Färg: Svart, Vit, Rosa
+//    Form: Rund, Droppformad
+//    Typ: Sötvatten, Saltvatten
+//
+// 2. När pärlan väl är skapad så ska man inte kunna ändra den.
+//
+// 4. Skapa ett lista bestående av 10 pärlor av slumpmässig storlek, färg
+//    form, och typ
+//
+// 5. Skriv ut listan
