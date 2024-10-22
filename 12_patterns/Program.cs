c2cfdfb6136bbf2fbd6d4d6460441552e8885dc3
Martin <martin_lenart@icloud.com>
2024-10-22 09:24:21 +0200

    update

--- 12_patterns/Program.cs
+++ 12_patterns/Program.cs
@@ -4,3 +4,18 @@ using Seido.Utilities.SeedGenerator;
 Console.WriteLine("12_patterns!");
 var _seeder = new csSeedGenerator();
 
+
+/* Exercises 
+
+1. Implement copy contructors on csPearl and csNecklace
+2. Implement a class factory that creates a Zebra necklace och black and white pearls.
+   One large black pearls (20mm) followed by two small white pearls (10mm) in a repetive pattern for X nr of pearls
+
+3. Implement a method AddFirst(csPearl p), AddLast(csPearl p),  RemoveFirst(csPearl p), RemoveLast(csPearl p) that
+   that uses Fluent syntax.
+4. Use the methods to change the first two pearls and last two pearls to two small pink (3mmm) pearls
+
+5. Implement csNecklace to be a Singleton, i.e ones a Zebra necklace is created all other calls to to 
+   Zebra necklace shall give the same instance
+
+*/ 
